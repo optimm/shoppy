533bff4ca2c296358248aa3e9ef08b1db781d58a
Bhawna147 <Khatribhawna77@gmail.com>
2021-09-16 22:01:45 +0530

    women page initial

--- shoppi/src/components/Women.js
+++ shoppi/src/components/Women.js
@@ -1,12 +1,64 @@
 import React from "react";
 import Navigation from "./Navigation";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container ,Row , Col ,Image} from "react-bootstrap";
+import "./Women.css";
+
 
 const Women = (props) => {
   console.log(props.location.state);
   return (
     <>
       <Navigation />
-      <h1>Hello this is Women fahion page</h1>
+
+   
+      <Row >
+      <Col lg={3} className="women-left" >
+
+     
+
+      <div className="women-filterBox">
+
+      <h2>Women Fashion</h2>
+ 
+       
+       <Col className= "women-filter">
+       <Row className="women-sort1" >
+
+         <h4>Sort By:- </h4>
+
+         </Row>
+         <Row  className="women-sort2" >
+         <h4>Sort By:- </h4>
+         </Row>
+         </Col>
+
+
+     
+
+
+        </div> 
+
+
+     
+      
+    
+      </Col>
+       
+
+
+
+      <Col  lg={9} className="women-right"  >
+        right
+      
+      </Col>
+
+
+
+      </Row>      
+
+     
+
     </>
   );
 };
