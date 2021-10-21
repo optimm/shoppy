91b769007f58e7d572dacb607db020ff8732f546
optimm <ayushsaxena823@gmail.com>
2021-10-21 17:46:25 +0530

    admin add product

--- shoppi/src/components/admin/AddProd.js
+++ shoppi/src/components/admin/AddProd.js
@@ -1,75 +1,103 @@
-import React from 'react'
-
+import React from "react";
+import { Delete, Update, AccountCircle, Add } from "@material-ui/icons";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import Particles from "react-particles-js";
 const AddProd = () => {
-    return (
-<div class="admin">
-
+  return (
+    <>
+      <div className="admin-nav">
+        <h1>Admin </h1>
+        <button className="admin">
+          <AccountCircle />
+        </button>
+      </div>
+      <div class="admin-a">
         <div className="admin-add">
-            <h1>ADD DETAILS</h1>
-
-
-            <h2>Product ID</h2>
-            <input className="admin-add-inp" 
-            type="text"
-            placeholder="Enter Product ID"
-            name = "p_ID"
-                        /> 
-             
-
-<br />
-<h2>Product Name</h2>
-            <input className="admin-add-inp" 
-            type="text"
-            placeholder="Enter Product Name"
-            name = "p_naem"
-                        /> 
-<br />
-
-<h2>Product Price</h2>
-            <input className="admin-add-inp" 
-            type="text"
-            placeholder="Enter Product Price"
-            name = "p_price"
-                        /> 
-<br />
-
-<h2>Product Description</h2>
-            <input className="admin-add-inp" 
-            type="text"
-            placeholder="Enter Product Description"
-            name = "p_ID"
-                        /> 
-
-<br />
-<h2>Product Category</h2>
-            <input className="admin-add-inp" 
-            type="text"
-            placeholder="Enter Product Category"
-            name = "p_ID"
-                        /> 
-
-<br />
-<h2>Product Type</h2>
-            <input className="admin-add-inp" 
-            type="text"
-            placeholder="Enter Product Type"
-            name = "p_ID"
-                        /> 
-
-<br />
-<h2>Product Image</h2>
-            <input className="admin-add-inp" 
-            type="text"
-            placeholder="Enter Product Imgae"
-            name = "p_ID"
-                        /> 
-
-            <button className="add-btn">Add</button></div>
-
+          <div className="admin-add-head">
+            <h2>Add new product</h2>
+          </div>
+          <div className="admin-add-data">
+            <Row>
+              <Col lg={6} md={6} sm={6}>
+                <input
+                  className="admin-add-input"
+                  type="text"
+                  placeholder="Enter product name"
+                  required
+                />
+              </Col>
+              <Col lg={6} md={6} sm={6}>
+                <input
+                  className="admin-add-input"
+                  type="text"
+                  pattern="\d*"
+                  placeholder="Enter product Price"
+                  required
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={6} md={6} sm={6}>
+                <input
+                  className="admin-add-input"
+                  type="text"
+                  placeholder="Enter product type"
+                  required
+                />
+              </Col>
+              <Col lg={6} md={6} sm={6}>
+                <select name="category" className="admin-add-input" required>
+                  <option
+                    selected
+                    disabled
+                    hidden
+                    className="add-category-head"
+                  >
+                    Select category
+                  </option>
+                  <option value="men">Men</option>
+                  <option value="women">Women</option>
+                  <option value="kids">Kids</option>
+                </select>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={12}>
+                <input
+                  className="admin-add-input url"
+                  type="text"
+                  placeholder="Enter product Image Url"
+                  required
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={12}>
+                <input
+                  className="admin-add-input url"
+                  type="text"
+                  placeholder="Enter product description"
+                  required
+                />
+              </Col>
+              <Col lg={12}>
+                <p className="admin-add-input url p-id">{`Product id = ${1}`}</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={6} md={6} sm={6} xs={6}>
+                <button className="admin-add-button">Preview</button>
+              </Col>
+              <Col lg={6} md={6} sm={6} xs={6}>
+                <button className="admin-add-button">Add</button>
+              </Col>
+            </Row>
+          </div>
+          <div className="admin-add-foot"></div>
         </div>
-      
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default AddProd
-
+export default AddProd;
--- shoppi/src/components/admin/Admin.js
+++ shoppi/src/components/admin/Admin.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 import "./admin.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { useState, useEffect } from "react";
-import { Delete , Update , AccountCircle , Add } from '@material-ui/icons';
+import { Delete, Update, AccountCircle, Add } from "@material-ui/icons";
 import createNotification from "../notification/notification";
 import "../product/prod.css";
 import {
@@ -18,26 +18,19 @@ import Axios from "axios";
 Axios.defaults.withCredentials = true;
 
 const Admin = () => {
+  const history = useHistory();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    Axios.post("http://localhost:8000/data", {
+      category: "men",
+    }).then((response) => {
+      setData(response.data);
+    });
+  }, []);
 
-    const history = useHistory();
-    const [data, setData] = useState([]);
-    useEffect(() => {
-      Axios.post("http://localhost:8000/data", {
-        category: "men",
-      }).then((response) => {
-        setData(response.data);
-      });
-    }, []);
-  
-
-    const refreshPage = ()=>{
-      window.location.reload();
-   }
-  
-
-
-
-
+  const refreshPage = () => {
+    window.location.reload();
+  };
 
   ///////// delete ////////////////
 
@@ -55,27 +48,22 @@ const Admin = () => {
       // showdata();
     }
   }
-   
 
-
-
-
-    return (
-        <>
-        
-        <div className="admin-nav">
+  return (
+    <>
+      <div className="admin-nav">
         <h1>Admin </h1>
-        <button className="admin"> <AccountCircle /> </button>
-        </div>
+        <button className="admin">
+          {" "}
+          <AccountCircle />{" "}
+        </button>
+      </div>
 
-        <Row>
-        <Col lg={9} md={12} sm={12} >
-
-
+      <Row>
+        <Col lg={9} md={12} sm={12}>
           <div className="product-left-box" id="scroll">
             <div className="product-cloth">
-        
-            {data.map((item, index) => {
+              {data.map((item, index) => {
                 if (item.p_category === "men") {
                   return (
                     <div className="category-section" key={index}>
@@ -92,27 +80,26 @@ const Admin = () => {
                             Rs. {item.p_price}
                           </span>
                         </p>
-
                       </div>
 
-
-                         <div className="admin-buttons">
-                          <button className="admin-btn">
-                              <Update/>
-                          </button>
-                          <button className="admin-btn"  onClick={() =>{ adminDel(index) ; refreshPage() } }  >
-                              <Delete/>
-                          </button>
-                          </div>
+                      <div className="admin-buttons">
+                        <button className="admin-btn">
+                          <Update />
+                        </button>
+                        <button
+                          className="admin-btn"
+                          onClick={() => {
+                            adminDel(index);
+                            refreshPage();
+                          }}
+                        >
+                          <Delete />
+                        </button>
+                      </div>
                     </div>
                   );
                 }
               })}
-
-                  
-    
-  
-              
             </div>
           </div>
         </Col>
@@ -120,67 +107,30 @@ const Admin = () => {
         {/* right */}
 
         <Col lg={3} md={12} sm={12} className="admin-right">
-       
-       
-
           <div className="admin-filterBox">
-
-
             <Col className="admin-filter-category">
-      
-            <h2>Category</h2>
+              <h2>Category</h2>
               <div className="sort-category">
-             
+                <input type="radio" id="" name="category" value="" />
+                <label for="">Mens</label>
+                <br />
+                <input type="radio" id="" name="category" value="" />
+                <label for="">Womens</label>
+                <br />
+                <input type="radio" id="" name="category" value="" />
+                <label for="">Kids</label>
+                <br />
+                <button className="admin-view">View</button>
+              </div>
 
-               <input
-                 type="radio"
-                 id=""
-                 name="category"
-                 value=""
-                 
-               />
-               <label for="">Mens</label>
-               <br />
-               <input
-                 type="radio"
-                 id=""
-                 name="category"
-                 value=""
-                 
-               />
-               <label for="">Womens</label>
-               <br />
-               <input
-                  type="radio"
-                  id=""
-                  name="category"
-                  value=""
-                 
-               />
-               <label for="">Kids</label>
-               <br />
-               <button className="admin-view" >
-                 View
-               </button>
-             </div>
-
-
-
-
-
-               
-
-               <div  className="admin-add-product">
-                  <Link to="/adminadd" className="addProd-btn">
-                  <Add/>
+              <div className="admin-add-product">
+                <Link to="/adminadd" className="addProd-btn">
+                  <Add />
                   Add product
-                  </Link>
-                   </div> 
+                </Link>
+              </div>
 
-
-
-
-{/* 
+              {/* 
                    <h2>Order Placed</h2>
 
 <div className="admin-order" id="scroll">
@@ -207,20 +157,12 @@ const Admin = () => {
 
   
 </div> */}
-
-
-
-
-
-
-             
             </Col>
           </div>
         </Col>
       </Row>
-
-      </>
-    );
+    </>
+  );
 };
 
 export default Admin;
